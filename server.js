8b9a43ea5c06b5186b42a8a5580046be947ed99c
Francesco Commisso <francescocommissodev@gmail.com>
2019-02-20 14:10:00 -0500

    everuthing

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("public/public"));
 
 app.listen(process.env.PORT || 8080, () => console.log("All is Ok bbg"));
