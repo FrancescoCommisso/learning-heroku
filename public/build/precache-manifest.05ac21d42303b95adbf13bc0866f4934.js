6533df9644f7c748e64187c229d7f60d9a1eaf3a
Francesco Commisso <francescocommissodev@gmail.com>
2019-02-20 14:23:42 -0500

    everuthing

---
+++ public/build/precache-manifest.05ac21d42303b95adbf13bc0866f4934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a10f077052ffc927a5cf",
+    "url": "/static/js/main.a10f0770.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "a10f077052ffc927a5cf",
+    "url": "/static/css/main.d2847835.chunk.css"
+  },
+  {
+    "revision": "8ed96f1b5c9651c7faa12c695f8dea4c",
+    "url": "/index.html"
+  }
+];
