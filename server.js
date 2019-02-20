ee373f15e776bb641e71ccca03a8bd30f6a465e1
Francesco Commisso <francescocommissodev@gmail.com>
2019-02-20 14:00:05 -0500

    added

--- server.js
+++ server.js
@@ -3,8 +3,5 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public/build"));
-app.get("/", function(req, res) {
-  res.render("index", {});
-});
 
 app.listen(process.env.PORT || 8080, () => console.log("All is Ok bbg"));
