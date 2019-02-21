d2d2c9885fa1cfd0dd0a9ade76df22adb8e44f10
Francesco Commisso <francescocommissodev@gmail.com>
2019-02-21 11:43:40 -0500

    email kinda working

--- server/server.js
+++ server/server.js
@@ -7,20 +7,27 @@ app.use(express.static("public/build"));
 app.listen(process.env.PORT || 8080, () => console.log("All is Ok bbg"));
 
 const nodemailer = require("nodemailer");
+var smtpTransport = require("nodemailer-smtp-transport");
+
 const auth = require("xoauth2");
 
-var transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    xoauth2: auth.createXOAuth2Generator({
-      user: "francescocommisso1996@gmail.com",
-      clientId:
-        "855271108184-h38e0772kqe9g0hdlgnlciggi2n1kvto.apps.googleusercontent.com",
-      clientSecret: "poT0BOP9R6bZm49tV6yB4_hP",
-      refreshToken: "http://developers.google.com/oauthplayground"
-    })
-  }
-});
+var transporter = nodemailer.createTransport(
+  smtpTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+      xoauth2: auth.createXOAuth2Generator({
+        user: "francescocommisso1996@gmail.com",
+        clientId:
+          "855271108184-h38e0772kqe9g0hdlgnlciggi2n1kvto.apps.googleusercontent.com",
+        clientSecret: "poT0BOP9R6bZm49tV6yB4_hP",
+        refreshToken: "1/sBwZh187i2kcsrpfwy-Ns1RFmc8eyAGyGqv6jZZFceg",
+        accessToken:
+          "ya29.Glu3BuKDf0V7QRL0qT-rX2rj5rNpFuo7OOtb_VXnKgpKu6ggRt4ER7qdTjXut52dOeEqIonYDE7jaR30w8M1a03z2HgfycaM50OqVTOLcIYnUfxx06ozTNq2ODlM"
+      })
+    }
+  })
+);
 
 var mailOptions = {
   from: "Francesco <francescocommisso1996@gmail.com>",
@@ -31,7 +38,7 @@ var mailOptions = {
 
 transporter.sendMail(mailOptions, function(err, res) {
   if (err) {
-    console.log("Mail Error");
+    console.log(err.message);
   } else {
     console.log("Email Sent!");
   }
