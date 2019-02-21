59915fdbd823eab1990bbf89fa4aab2ec29b9bae
Francesco Commisso <francescocommissodev@gmail.com>
2019-02-21 15:50:37 -0500

    debugging on master like a boss

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+var cors = require("cors");
 
 const app = express();
 
 app.use(express.static("public/build"));
+app.use(cors());
 
 const mailer = require("./mailer.js");
 
