f9e093127db71386b2a7b96657249d9199ca309c
Francesco Commisso <francescocommissodev@gmail.com>
2019-02-25 13:33:36 -0500

    added git ignore

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
-// app.use(bodyParser);
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static("public/build"));
