be836cb646222fe0e2ddd21a5e79371e856eeb44
Francesco Commisso <francescocommissodev@gmail.com>
2019-02-25 14:04:47 -0500

    added scripts

--- server/mailer.js
+++ server/mailer.js
@@ -3,7 +3,7 @@ var smtpTransport = require("nodemailer-smtp-transport");
 
 const auth = require("xoauth2");
 
-//TestY TEST TEST U SHOULDNT SEE ME NANANA BOOOBOO
+//Am I here?
 
 var transporter = nodemailer.createTransport(
   smtpTransport({
